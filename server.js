f9fff72ae87266f4b9097021bca9908cab4e6980
Steven Johnson <sjohnson21960@gmail.com>
2020-07-17 18:29:48 -0500

    Home page and note page routes

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+  });
+  
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+  app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "db.json"));
+  });
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
