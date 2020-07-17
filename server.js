1b861b7261322306153cc982713dafc50be8e7de
Steven Johnson <sjohnson21960@gmail.com>
2020-07-17 18:57:50 -0500

    update to server functionality

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var path = require("path");
 
+//----Express app set up
 var app = express();
 var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//----HTML routes
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
   });
@@ -19,6 +21,11 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "db.json"));
   });
 
+  app.get("/api/notes", function(req, res) {
+    res.json(tableData);
+  });
+
+  //---starting server
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
