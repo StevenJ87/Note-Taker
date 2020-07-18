5b10d12d144a2ff299c0b3946a26c628493119b6
Steven Johnson <sjohnson21960@gmail.com>
2020-07-18 00:49:41 -0500

    Finalizing note functionality and adding id number

--- server.js
+++ server.js
@@ -29,15 +29,16 @@ app.get("/notes", function(req, res) {
   });
 
   app.post("/api/notes",function(req,res){
+    let notes = JSON.parse(fs.readFileSync(path.join(__dirname, "/Public/db.json"), "utf-8")); 
     let newNote = req.body;
     notes.push(newNote);
-    notes.forEach((note)=>{
-        let i = 0;
+    let i = 1;
+    notes.forEach(note=>{
         note.id = i;
         i++;
     });
-    console.log(newNote);
-    fs.writeFile(path.join(__dirname,"/db.json"), JSON.stringify(notes),function (err){
+    console.log(notes);
+    fs.writeFile(path.join(__dirname,"/Public/db.json"), JSON.stringify(notes),function (err){
         if (err) throw err;
         res.json(true);
         return res.end();
