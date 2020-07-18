d7e40f8b2e8f51c752e681557c4ad39a7d921db4
Steven Johnson <sjohnson21960@gmail.com>
2020-07-17 22:36:17 -0500

    post call updated

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require("express");
 var path = require("path");
+const { fstat } = require("fs");
 
 //----Express app set up
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//----Notes array
+let notes =[];
 
 //----HTML routes
 app.get("/notes", function(req, res) {
@@ -19,6 +23,22 @@ app.get("/notes", function(req, res) {
 
   app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "db.json"));
+  });
+
+  app.post("/api/notes",function(req,res){
+    let newNote = req.body;
+    notes.push(newNote);
+    notes.forEach((note)=>{
+        let i = 0;
+        note.id = i;
+        i++;
+    });
+    console.log(newNote);
+    fs.writeFile(path.join(__dirname,"db.json"), JSON.stringify(notes),function (err){
+        if (err) throw err;
+        res.json(true);
+        return res.end();
+    })
   });
 
   //---starting server
